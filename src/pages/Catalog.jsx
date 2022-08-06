4b85235a73cf293f2740093f85e4ac829c3e4dce
tranxuantruongjk <xuantruonghy823@gmail.com>
2022-08-06 14:31:43 +0700

    make movie-grid

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import PageHeader from "../components/page-header/PageHeader";
 
 import { category as cate} from "../api/tmdbApi"
+import MovieGrid from "../components/movie-grid/MovieGrid";
 
 const Catalog = () => {
 
@@ -15,6 +16,11 @@ const Catalog = () => {
             <PageHeader>
                 {category === cate.movie ? 'Movies' : 'TV Series'}
             </PageHeader>
+            <div className="container">
+                <div className="section mb-3">
+                    <MovieGrid category={category} />
+                </div>
+            </div>
         </>
     )
 }
